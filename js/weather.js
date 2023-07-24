1846a7f982dc856161696f2585a638ec3db5a237
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-07-24 11:23:11 -0500

    changed element positions

--- js/weather.js
+++ js/weather.js
@@ -46,7 +46,7 @@ weather.addEventListener('submit', (e) => {
     const cityState = document.querySelector('.city-state');
     const app = document.querySelector('.app');
 
-    app.style.height = '200px';
+    app.style.height = '180px';
 
     cityState.innerText = `${weatherInfo.city}, ${weatherInfo.state}`;
 
