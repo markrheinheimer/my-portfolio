affd04dee0104f8f5d52b2f6e80f2a3f6d876bae
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2024-01-30 12:40:49 -0600

    changed fetch

--- functions/weatherfetch/weatherfetch.cjs
+++ functions/weatherfetch/weatherfetch.cjs
@@ -1,5 +1,5 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 // functions/weatherfetch/weatherfetch.cjs
+const fetch = require('node-fetch');
 
 const handler = async (event) => {
   try {
@@ -12,9 +12,8 @@ const handler = async (event) => {
     // Make a request to the weather API
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
 
-    // Dynamic import of node-fetch
-    const fetchModule = await import('node-fetch');
-    const apiResponse = await fetchModule.default(apiUrl);
+    // Use the fetch function from the 'node-fetch' module
+    const apiResponse = await fetch(apiUrl);
 
     if (!apiResponse.ok) {
       throw new Error(
